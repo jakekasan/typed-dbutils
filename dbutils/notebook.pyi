68c15a1a604d78827e400e9d2ed04861201b814f
jakekasan <jake.kasan@gmail.com>
2022-11-22 21:49:29 +0000

    Fix argument name

--- dbutils/notebook.pyi
+++ dbutils/notebook.pyi
@@ -2,7 +2,7 @@
 from typing import Any, Dict
 
 
-def run(path: str, timeoutSeconds: int, arguments: Dict[Any, Any]) -> str:
+def run(path: str, timeout_seconds: int, arguments: Dict[Any, Any]) -> str:
     ...
 
 def exit(value: str) -> None:
